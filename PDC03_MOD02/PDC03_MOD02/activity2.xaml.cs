952a69d7ca79866329ac1893f182428c119c8002
kathmrcdo <83403685+kathmrcdo@users.noreply.github.com>
2022-04-26 17:37:12 +0800

    Add files via upload

---
+++ PDC03_MOD02/PDC03_MOD02/activity2.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MOD02
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class activity2 : ContentPage
+    {
+        public activity2()
+        {
+            InitializeComponent();
+        }
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            valuelabel.Text = args.NewValue.ToString("F3");
+        }
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            await DisplayAlert("Clicked!", "The button'" + button.Text + "' has been clicked", "OK");
+
+        }
+    }
+}
